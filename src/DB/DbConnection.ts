be8d1f258fa6c4e6aa9e803d585e14a2b8db9533
Arvind054 <arvindchoudhary054@gmail.com>
2026-07-23 19:52:59 +0530

    Helper Function to Connect to DB

--- src/DB/DbConnection.ts
+++ src/DB/DbConnection.ts
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
 
 // Function to Connect to the DB
-connectDB()
-.then(()=>console.log("DB connected Successfully"))
-.catch(err=>console.log("Error Occured: ", err))
 
-async function connectDB(){
+
+async function connectToDB(){
     const dbUrl = process.env.DB_URL || "";
     if(!dbUrl){
        throw new Error("Connection URL not Found");
     }
     await mongoose.connect(dbUrl);
+}
+
+export async function connectDB(){
+    try{
+       await connectToDB();
+    }catch(err){
+        console.log("Error Connecting to DB", err);
+    }
 }
