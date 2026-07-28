8191db6f0c21250ce04ffa0e746a9c819883e568
Arvind054 <arvindchoudhary054@gmail.com>
2026-07-28 09:22:50 +0530

    Fixed mongoDB adapter issue

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -3,16 +3,7 @@ import {MongoClient} from "mongodb";
 const DB_URL = process.env.DB_URL!;
 
 const client = new MongoClient(DB_URL);
+await client.connect();
 
-let promise : Promise<MongoClient>;
-
-if(process.env.NODE_ENV == "development"){
-    if (!(global as any)._mongoClientPromise) {
-        (global as any)._mongoClientPromise = client.connect();
-    }
-    promise = (global as any)._mongoClientPromise;
-}else{
-    promise = client.connect();
-}
-
-export default promise;
+const db = await client.db();
+export default db;
