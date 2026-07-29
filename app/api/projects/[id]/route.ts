f0109ee5b22abcfd47f0e01436179520a396fb50
Arvind054 <arvindchoudhary054@gmail.com>
2026-07-29 11:40:57 +0530

    Cascsde delete and Regenerate API Key

--- app/api/projects/[id]/route.ts
+++ app/api/projects/[id]/route.ts
@@ -1,5 +1,6 @@
 import { auth } from "@/lib/auth";
 import { generate_API_Key } from "@/lib/generate_api_key";
+import { Incident } from "@/src/DB/models/incidentSchemas";
 import { Log } from "@/src/DB/models/logSchema";
 import { Project } from "@/src/DB/models/projectSchema";
 import { headers } from "next/headers";
@@ -51,6 +52,8 @@ try{
       }
       const {projectId} = await params;
       if(!projectId)return NextResponse.json({error: "Project Id Not Found"}, {status: 400});
+        await Log.deleteMany({projectId});
+        await Incident.deleteMany({projectId});
        await Project.findByIdAndDelete(projectId);
       return NextResponse.json({message: "Project Deleted Successfully"}, {status: 201});
    }catch(err){
@@ -59,3 +62,20 @@ try{
    }
 };
 
+// To Regenerathe the API Key
+export async function POST(req: NextRequest, {params} :{params: Promise<{projectId: String}>}){
+     try{
+      const session = await auth.api.getSession({headers: await headers()});
+      if(!session?.user?.id){
+        return NextResponse.json({error: "Unauthorized"}, {status: 401});
+      }
+      const {projectId} = await params;
+      if(!projectId)return NextResponse.json({error: "Project Id Not Found"}, {status: 400});
+      const newApiKey = generate_API_Key();
+      const project = await Project.findByIdAndUpdate(projectId,{apiKey: newApiKey});
+      return NextResponse.json({project}, {status: 201});
+   }catch(err){
+        console.log("Error: ", err);
+        return NextResponse.json({error: "Error Getting Project"}, {status:500});
+   }
+}
